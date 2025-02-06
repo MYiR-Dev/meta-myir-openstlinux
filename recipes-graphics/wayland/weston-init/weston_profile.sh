32533e3516b76a27942d9d744f40c2e7fbf486c1
beste <beste.wang@myir.cn>
2025-02-06 10:40:58 +0800

    FEAT: Modify Weston settings and enable Wayland platform for QT
    - Updated weston.ini to change background color and disable panel
    - Commented out default background image and launcher configuration
    - Added QT_QPA_PLATFORM=wayland in weston_profile.sh for Wayland support

--- recipes-graphics/wayland/weston-init/weston_profile.sh
+++ recipes-graphics/wayland/weston-init/weston_profile.sh
@@ -6,6 +6,7 @@ export ECORE_EVAS_ENGINE=wayland_shm
 export GDK_BACKEND=wayland
 export PULSE_RUNTIME_PATH=/run/user/`id -u weston`/pulse
 export USE_PLAYBIN3=1
+export QT_QPA_PLATFORM=wayland
 
 export WAYLAND_DISPLAY=wayland-0
 if [ -e $XDG_RUNTIME_DIR/wayland-1 ]; then
